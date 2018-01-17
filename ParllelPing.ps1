abcf515703d0ffca1604c0ba8529cae9ec47f307
nik2017 <nikhil.eltrx@gmail.com>
2018-01-17 23:31:54 +1100

    PingParllel

---
+++ ParllelPing.ps1
@@ -0,0 +1,59 @@
+﻿<#
+This pull the list ip fro $ipa file and . Start concurren continous ping on all ip and wite the output in file $locn\output
+On the Powershell windows, "Press eneter to continue show". Once enter is pressed, all PING process will be killed.  
+
+#>
+
+cls
+$ips= Get-Content "c:\tmp\ips.txt"
+$ips
+$locn= "c:\tmp\"
+cd $locn
+New-Item outPut -ItemType Directory -EA SilentlyContinue
+Set-Location $locn"outPut"
+
+$runspacepool =  [RunSpaceFactory]::CreateRunspacePool(1,10)
+$runspacepool.ApartmentState = "MTA"
+$runspacepool.open()
+
+$codeContainer = {
+    param (
+            [string] $ipAddr
+            )
+            $FileName = Get-Date -Format yyMMddHHmmss
+            filter timestamp {"$(Get-Date -Format o): $_"}
+            $pingst= ping $ipAddr -t | timestamp | Out-File c:\tmp\outPut\"$FileName"_"$ipAddr"_p.txt -Force
+            return $pingst
+}
+
+$threads = @()
+
+foreach ($i in $ips)
+{ 
+    $runspaceObject = [PSCustomObject] @{
+        Runspace = [PowerShell]::Create()
+        Invoker= $null
+    }
+    $runspaceObject.Runspace.RunSpacePool = $runspacepool
+    $runspaceObject.Runspace.AddScript($codeContainer) | Out-Null
+    $runspaceObject.Runspace.AddArgument($i)| Out-Null
+    $runspaceObject.Invoker = $runspaceObject.Runspace.BeginInvoke()
+    $threads += $runspaceObject
+
+    Write-Output "Finished creating runspace for $i"
+}
+#Get-Process PING
+pause
+kill -ProcessName PING -Force
+while ($threads.Invoker.IsCompleted -contains $false){}
+Write-Output "All completed"
+
+$threadResult = @()
+ForEach($t in $threads)
+{
+    $threadResult += $t.Runspace.EndInvoke($t.Invoker)
+    $t.Runspace.Dispose()
+}
+
+$runspacepool.Close()
+$runspacepool.Dispoer
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PowerShell
 ## TcketTree.ps1
-	This script will create a folder $baseLocation/Year/Month/6digitTicketnumber if the ticket folder is not created in last three months. The folder will be opened and put path folder will be added to windows clip-board(i.e "Ctl+v" will paste the full patch of the ticket).It can also list the file from Doanload Directory if any file contain the ticket number.It will allow to copy the file to ticket directory just by intering index number of the file seprated by comma.
+	This script will create a folder $baseLocation/Year/Month/6digitTicketnumber if the ticket folder is not created in last three months.
+	The folder will be opened and put path folder will be added to windows clip-board(i.e "Ctl+v" will paste the full patch of the ticket).
+	It can also list the file from Doanload Directory if any file contain the ticket number.
+	It will allow to copy the file to ticket directory just by intering index number of the file seprated by comma.
+
+##ParllelPing	
+	This pull the list ip fro $ipa file and . Start concurren continous ping on all ip and wite the output in file $locn\output. On the Powershell windows, "Press eneter to continue show". Once enter is pressed, all PING process will be killed. 
